6aef95339149b33e4203fb7b7cd83009f0330dcb
Pradeep Kumar <prad.sarkar@gmail.com>
2018-03-02 01:18:09 +0530

    Profile Page improvisations

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { ViewChild } from '@angular/core';
 import { Slides } from 'ionic-angular';
@@ -15,11 +15,28 @@ import { Slides } from 'ionic-angular';
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit {
+  profiles: { fullName: string; imageSrc: string; }[];
   currentIndex: number;
   @ViewChild(Slides) slides: Slides;
 
   constructor() {
+    
+  }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.profiles = [
+      {
+        fullName:"Vivaan Gatadi Vadda",
+        imageSrc:'./../../assets/imgs/4.jpg'
+      },
+      {
+        fullName:"Viha Gatadi Vadda",
+        imageSrc:'./../../assets/imgs/2.jpeg'
+      }
+    ]
   }
 
   goToSlide() {
